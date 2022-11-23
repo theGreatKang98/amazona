d2a35063b077813c248aed2376745c578d19b808
theGreatKang98 <srouc159@gamil.com>
2022-11-24 03:52:33 +0900

    display products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ProductItem from '../components/ProductItem'
 import styles from '../styles/Home.module.css'
+import data from '../utils/data'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
+    <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+      {data.products.map((product)=>(
+        <ProductItem product={product} key={product.slug} />
+      ))}
     </div>
   )
 }
