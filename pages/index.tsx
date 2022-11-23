c33697d426560a8c44a7d67b9e600d9665ae0abb
theGreatKang98 <srouc159@gamil.com>
2022-11-24 08:32:35 +0900

    product detail page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import data from '../utils/data'
 export default function Home() {
   return (
     <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
-      {data.products.map((product)=>(
+      {data.products.map((product) => (
         <ProductItem product={product} key={product.slug} />
       ))}
     </div>
